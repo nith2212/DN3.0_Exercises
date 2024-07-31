e59abf61a5c8def79860efcbcccd4b441a0d0c56
Nithyasri Sukumaran <157895664+nith2212@users.noreply.github.com>
2024-07-31 13:36:51 +0530

    Create ConcreteDecorators.java

---
+++ Week1/Design_Patterns_And_Principles/Exercise5/ConcreteDecorators.java
@@ -0,0 +1,31 @@
+public class SMSNotifierDecorator extends NotifierDecorator {
+    public SMSNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        sendSMS(message);
+    }
+
+    private void sendSMS(String message) {
+        System.out.println("Sending SMS with message: " + message);
+    }
+}
+
+public class SlackNotifierDecorator extends NotifierDecorator {
+    public SlackNotifierDecorator(Notifier notifier) {
+        super(notifier);
+    }
+
+    @Override
+    public void send(String message) {
+        super.send(message);
+        sendSlack(message);
+    }
+
+    private void sendSlack(String message) {
+        System.out.println("Sending Slack message: " + message);
+    }
+}
